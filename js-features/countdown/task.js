a9ab22de49faf54b1e74b8351fbb9aac73ddd544
striiker410 <striiker410@gmail.com>
2023-05-30 14:05:14 +0300

    Задание выполнено

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,1 +1,12 @@
 //Задание №1
+const timer = document.getElementById('timer');
+
+const timerId = setInterval(() => {
+    const value = timer.textContent;
+    timer.textContent = value - 1;
+
+    if (value == 1) {
+        alert('Вы победили в конкурсе!');
+        clearInterval(timerId);
+    }
+}, 100);
