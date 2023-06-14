d116704de19ae7986d4720884cf3c0efdad23b6b
striiker410 <striiker410@gmail.com>
2023-06-14 12:06:17 +0300

    Задание №3 выполнено

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,18 @@
+//Задание №3
+
+const textArea = document.querySelector('#editor');
+const clearText = document.querySelector('#button');
+const storedText = localStorage.getItem('textInTextarea');
+
+if(storedText) {
+   textArea.value = storedText;  
+  }
+
+textArea.onkeyup = () => {
+  localStorage.setItem('textInTextarea', textArea.value); 
+}
+
+clearText.addEventListener('click', () => {
+  localStorage.removeItem('textInTextarea');
+  textArea.value = '';
+})
